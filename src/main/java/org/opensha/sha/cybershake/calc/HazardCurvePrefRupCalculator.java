c97d42549abaad80ae8882a8461071dd79535366
Akash Bhatthal <akashbhatthal@gmail.com>
2025-04-29 16:17:39 -0700

    Update readRupVarCSV for new CSV format. Allows for grouping variations by corresponding source+rupture.

--- src/main/java/org/opensha/sha/cybershake/calc/HazardCurvePrefRupCalculator.java
+++ src/main/java/org/opensha/sha/cybershake/calc/HazardCurvePrefRupCalculator.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -15,14 +18,20 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.opensha.commons.util.ClassUtils;
+import org.opensha.commons.util.ExceptionUtils;
 import org.opensha.commons.data.CSVReader;
 import org.opensha.commons.data.CSVReader.Row;
 
 import org.opensha.sha.cybershake.constants.CyberShakeStudy;
 import org.opensha.sha.cybershake.db.CybershakeIM;
 import org.opensha.sha.cybershake.db.CybershakeRun;
+import org.opensha.sha.cybershake.db.Cybershake_OpenSHA_DBApplication;
+import org.opensha.sha.cybershake.db.DBAccess;
+import org.opensha.sha.cybershake.db.PeakAmplitudesFromDB;
+import org.opensha.sha.cybershake.db.PeakAmplitudesFromDBAPI;
 import org.opensha.sha.cybershake.plot.HazardCurvePlotter;
 
 import com.google.common.base.Joiner;
@@ -34,15 +43,31 @@ import com.google.common.base.Preconditions;
  */
 public class HazardCurvePrefRupCalculator implements RuptureVariationProbabilityModifier {
 
-	File rvProbsCSV;
-	Map<Integer, Double> ruptureVariationBiases;
+
+	// Maps a <Source_ID + Rupture_ID> composite key to a Set of <Rup_Var_ID + Probability>
+	// Rupture variations are not globally unique, but are unique to a given source and rupture.
+	// Each rupture variation from the CSV has a provided probability.
+	//
+	// Not all variations for a rupture may be specified, in which case they are dynamically
+	// calculated inside getVariationProbs. This means there will be realtime validation
+	// beyond the validation of CSV variationProbs.
+	private Map<ImmutablePair<Integer, Integer>, Set<ImmutablePair<Integer, Double>>> variationProbs;
+
+	private PeakAmplitudesFromDBAPI peakAmplitudes;
 	
+	private static final boolean D = true;
+
 	/**
 	 * Constructor parses command line parameters from main method and initializes
 	 * local parameters for use in HazardCurve computation.
+	 * @param cmd - Arguments received from user input at CommandLine
+	 * @param peakAmplitudes - Needed to get earthquake ruptures from database
 	 * @throws MissingOptionException 
 	 */
-	public HazardCurvePrefRupCalculator(CommandLine cmd) throws MissingOptionException {
+	public HazardCurvePrefRupCalculator(CommandLine cmd, PeakAmplitudesFromDBAPI peakAmplitudes)
+			throws MissingOptionException {
+		
+		this.peakAmplitudes = peakAmplitudes;
 		
 		// One of run-id, site-name, or site-id is required.
 		if (!(cmd.hasOption("run-id")
@@ -51,13 +76,44 @@ public class HazardCurvePrefRupCalculator implements RuptureVariationProbability
 			throw new MissingOptionException(
 					"One of run-id, site-name, or site-id is required.");
 		}
-		// TODO: Finish parsing the rest of the parameters and then set local vars.
+		
+		if (!cmd.hasOption("rv-probs-csv")) {
+			throw new MissingOptionException("Missing option rv-probs-csv (Rupture Variations CSV Input File)");
+		}
+		// TODO: Finish parsing the rest of the parameters
+
+		// TODO: Work on input probabilties validation
+		// TODO: Update the rupVarProbs Map to utilize residual probabilities
+
+		readRupVarCSV(new File(cmd.getOptionValue("rv-probs-csv")));
+		// TODO: Verify if the supplied probabilities are viable
+		//		* Do the sum of variation probabilties provided exceed the rupture probability? (read from DB)
+		// TODO: Calculate residual probabilities for non-specified rupVars
+		//		* (1 - probability-sum-from-csv) / (count-vars-from-db - variations-in-mapping-for-this-rupture)
+		// TODO: Move the peakAmplitudes reading into dedicated method for
+		//       populating the rupVarProbs mapping.
 	}
 
+	/**
+	 * Gets the probabilities of the rupture variations for the supplied rupture ID.
+	 * This takes into account the CSV file with overriden variation probabilities.
+	 * 
+	 * HazardCurve calc gets originalProb from DB.
+	 * Provided probabilities are validated to ensure they can sum to the originalProb 
+	 * with the remainder divided over the residual unspecified variations.
+	 * (Unless all variations were specified, in which case they must simply sum to the originalProb)
+	 * Otherwise we return a Runtime Exception.
+	 *
+	 */
 	@Override
 	public List<Double> getVariationProbs(int sourceID, int rupID, double originalProb, CybershakeRun run,
 			CybershakeIM im) {
 		// TODO Copy over from RupVarProbModifierTest but override rupVar probs
+		try {
+			System.out.println(peakAmplitudes.getIM_Values(run.getRunID(), sourceID, rupID, im));
+		} catch (SQLException e) {
+			throw ExceptionUtils.asRuntimeException(e);
+		}
 		return null;
 	}
 	
@@ -70,25 +126,37 @@ public class HazardCurvePrefRupCalculator implements RuptureVariationProbability
 	}
 	
 	/**
-	 * Reads rupture variation biases from a CSV file.
-	 * This static method can be used independently of the CLT (See main).
+	 * Populates maps by reading CSV file
+	 * CSV Headers: Source_ID,Rupture_ID,Rup_Var_ID,Probability
 	 * @param csvFile
-	 * @return
 	 */
-	private static Map<Integer, Double> readRupVarCSV(File csvFile) {
-		Map<Integer, Double> biasMap = new HashMap<>();
+	private void readRupVarCSV(File csvFile) {
+		variationProbs = new HashMap<>();
 		try (CSVReader reader = new CSVReader(new FileInputStream(csvFile))) {
 			// Validate headers
 			Row headers = reader.read();
-			Preconditions.checkArgument(headers.columns()==2, "Must have two columns in CSV map");
-			Preconditions.checkArgument(headers.get(0).equals("Rup_Var_ID"),
-					"First column header must be named `Rup_Var_ID`");
-			Preconditions.checkArgument(headers.get(1).equals("Prob"),
-					"Second column header must be named `Prob`");
+			Preconditions.checkArgument(headers.columns()==4, "Must have two columns in CSV map");
+			Preconditions.checkArgument(headers.get(0).equals("Source_ID"),
+					"First column header must be named `Source_ID`");
+			Preconditions.checkArgument(headers.get(1).equals("Rupture_ID"),
+					"Second column header must be named `Rupture_ID`");
+			Preconditions.checkArgument(headers.get(2).equals("Rup_Var_ID"),
+					"Third column header must be named `Rup_Var_ID`");
+			Preconditions.checkArgument(headers.get(3).equals("Probability"),
+					"Fourth column header must be named `Probability`");
 			// Read data line by line into map
 			for (Row row : reader) {
-				Preconditions.checkArgument(row.columns()==2, "Must have two columns in CSV map");
-				biasMap.put(row.getInt(0), row.getDouble(1));
+				Preconditions.checkArgument(row.columns()==4, "Must have four columns in CSV map");
+				ImmutablePair<Integer, Integer> compositeKey =
+						ImmutablePair.of(row.getInt(0), row.getInt(1)); 
+				ImmutablePair<Integer, Double> variationProb =
+						ImmutablePair.of(row.getInt(2), row.getDouble(3));
+
+				if (!variationProbs.containsKey(compositeKey)) {
+					variationProbs.put(compositeKey,
+							new HashSet<ImmutablePair<Integer, Double>>());
+				}
+				variationProbs.get(compositeKey).add(variationProb);
 			}
 		} catch (FileNotFoundException e) {
 			System.err.println("CSV File not found.");
@@ -100,7 +168,11 @@ public class HazardCurvePrefRupCalculator implements RuptureVariationProbability
 			System.err.println("Rupture variation biases CSV File encountered an IO exception.");
 			e.printStackTrace();
 		}
-		return biasMap;
+		if (D) {
+			for (var key : variationProbs.keySet()) {
+				System.out.println(key + " has " + variationProbs.get(key).size() + " variations");
+			}
+		}
 	}
 
 
@@ -165,8 +237,14 @@ public class HazardCurvePrefRupCalculator implements RuptureVariationProbability
 				HazardCurvePlotter.printUsage(options, appName);
 			}
 			
-			if (args.length == 1 && (args[0].endsWith("-help") || args[0].endsWith("-?")))
+			if (args.length == 1 && (args[0].endsWith("-help") || args[0].endsWith("-?"))) {
+				System.out.println("usage:\n"
+						+ "  --study <study enum name> --period <periods, comma separated>\n"
+						+ "  [--run-id <run id> OR --site-name <site short name> OR --site-id <site id>]\n"
+						+ "  --rv-probs-csv <path to CSV input file> --output-dir <output directory>\n");
+				System.out.println("One of the run-id, site-name, or site-id must be provided!\n");
 				HazardCurvePlotter.printHelp(options, appName);
+			}
 			
 			try {
 				CommandLine cmd = parser.parse(options, args);
@@ -174,29 +252,34 @@ public class HazardCurvePrefRupCalculator implements RuptureVariationProbability
 				if (cmd.hasOption("help") || cmd.hasOption("?")) {
 					HazardCurvePlotter.printHelp(options, appName);
 				}
-				HazardCurvePrefRupCalculator calc = new HazardCurvePrefRupCalculator(cmd);
+
+
+				DBAccess db = Cybershake_OpenSHA_DBApplication.getDB();
+				PeakAmplitudesFromDBAPI peakAmplitudes = new PeakAmplitudesFromDB(db);
+
+				// This calculator implements the RuptureVariationProbabilityModifier
+				// interface and acts as both the CLT and modification logic to the 
+				// HazardCurveComputation.
+				RuptureVariationProbabilityModifier rupVarModifier =
+						new HazardCurvePrefRupCalculator(cmd, peakAmplitudes);
+
+				HazardCurveComputation hazardCurveCalc = new HazardCurveComputation(db);
+				// Update the probability of each individual rupture variation
+				hazardCurveCalc.setRupVarProbModifier(rupVarModifier);
 				
-				// TODO: Load CSV input file
-				// TODO: Create instance of rupture calc with input file
-				// TODO: Create HazardCurveComputation instance to use rupture calc.
+				// TODO: Invoke hazardCurveCalc computeHazardCurve
 
 			} catch (MissingOptionException e) {
 				System.err.println(e.getMessage());
 				HazardCurvePlotter.printUsage(options, appName);
 			}
 			
-			System.out.println("Done!");
+			if (D) System.out.println("HazardCurvePrefRupCalculator Done!");
 			System.exit(0);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
-		// Test `getRupVarBiases` works
-//		System.out.println(
-//				HazardCurvePrefRupCalculator.getRupVarBiases(
-//						new File("/Users/bhatthal/git/opensha-cybershake-fork/rupVarBiases.csv")));
-		
 	}
 
 }

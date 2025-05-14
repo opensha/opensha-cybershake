758a4f0d57a9af8c318c8b5b964ee0b1bac74ee2
Kevin Milner <kmilner@usgs.gov>
2025-05-14 13:55:58 -0700

    now broken out by individual GMPE

--- src/main/java/scratch/kevin/cybershake/MultiFaultGMPE_Compare.java
+++ src/main/java/scratch/kevin/cybershake/MultiFaultGMPE_Compare.java
@@ -20,7 +20,12 @@ import scratch.kevin.bbp.BBP_SourceFile;
 public class MultiFaultGMPE_Compare {
 
 	public static void main(String[] args) throws IOException {
-		ScalarIMR gmpe = AttenRelRef.NGAWest_2014_AVG_NOIDRISS.get();
+		ScalarIMR[] gmpes = {
+				AttenRelRef.ASK_2014.get(),
+				AttenRelRef.BSSA_2014.get(),
+				AttenRelRef.CB_2014.get(),
+				AttenRelRef.CY_2014.get()
+		};
 		File dir = new File("/home/kevin/CyberShake/landers_multifault_calcs");
 		List<BBP_SourceFile> segments = List.of(
 				BBP_SourceFile.readFile(new File(dir, "landers_v16_08_1_seg1.src")),
@@ -37,8 +42,10 @@ public class MultiFaultGMPE_Compare {
 		CompoundSurface fullSurf = new CompoundSurface(segSurfaces);
 		EqkRupture rup = new EqkRupture(mag, rake, fullSurf, null);
 		
-		gmpe.setIntensityMeasure(SA_Param.NAME);
-		gmpe.setEqkRupture(rup);
+		for (ScalarIMR gmpe : gmpes) {
+			gmpe.setIntensityMeasure(SA_Param.NAME);
+			gmpe.setEqkRupture(rup);
+		}
 		double[] periods = {0.01, 0.02, 0.05, 0.075, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0, 7.5, 10.0};
 		
 		List<Site> sites = new ArrayList<>();
@@ -46,18 +53,29 @@ public class MultiFaultGMPE_Compare {
 			sites.add(site.buildGMPE_Site(null));
 		
 		for (double period : periods) {
-			SA_Param.setPeriodInSA_Param(gmpe.getIntensityMeasure(), period);
+			for (ScalarIMR gmpe : gmpes)
+				SA_Param.setPeriodInSA_Param(gmpe.getIntensityMeasure(), period);
 			File outputFile = new File(dir, "gmpe_results_"+(float)period+"s.csv");
 			CSVFile<String> csv = new CSVFile<>(true);
-			csv.addLine("Site Name", "Latitude", "Longitude", "GMPE ln mean", "GMPE sigma");
+			List<String> header = new ArrayList<>();
+			header.add("Site Name");
+			header.add("Latitude");
+			header.add("Longitude");
+			for (ScalarIMR gmpe : gmpes) {
+				header.add(gmpe.getShortName()+" ln mean");
+				header.add(gmpe.getShortName()+" sigma");
+			}
+			csv.addLine(header);
 			for (Site site : sites) {
-				gmpe.setSite(site);
 				List<String> line = new ArrayList<>(csv.getNumCols());
 				line.add(site.getName());
 				line.add((float)site.getLocation().lat+"");
 				line.add((float)site.getLocation().lon+"");
-				line.add((float)gmpe.getMean()+"");
-				line.add((float)gmpe.getStdDev()+"");
+				for (ScalarIMR gmpe : gmpes) {
+					gmpe.setSite(site);
+					line.add((float)gmpe.getMean()+"");
+					line.add((float)gmpe.getStdDev()+"");
+				}
 				csv.addLine(line);
 			}
 			

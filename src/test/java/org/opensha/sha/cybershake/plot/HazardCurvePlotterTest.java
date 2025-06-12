767d800f7f331a9f320e6500a143f952a1c46afa
Akash Bhatthal <akashbhatthal@gmail.com>
2025-06-12 09:50:23 -0700

    Add tests skeleton and current CSVs

--- src/test/java/org/opensha/sha/cybershake/plot/HazardCurvePlotterTest.java
+++ src/test/java/org/opensha/sha/cybershake/plot/HazardCurvePlotterTest.java
@@ -2,15 +2,45 @@ package org.opensha.sha.cybershake.plot;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.opensha.sha.cybershake.db.Cybershake_OpenSHA_DBApplication;
 
 public class HazardCurvePlotterTest {
 	
-	// -o /Users/bhatthal/git/opensha-cybershake-fork/src/test/resources/org/opensha/sha/cybershake/plot/HazardCurvePlotter  -R 215
-	
-	// -o /Users/bhatthal/git/opensha-cybershake-fork/src/test/resources/org/opensha/sha/cybershake/plot/HazardCurvePlotter  -R 215 -rv /Users/bhatthal/git/opensha-cybershake-fork/src/test/resources/org/opensha/sha/cybershake/plot/HazardCurvePlotter/USC_mod_probs_Mojave_Coachella.csv
+	// TODO: Fix these tests to use relative paths
+	final String RUN_ID = "9306";
+	final String RUP_VAR_CSV = "/Users/bhatthal/git/opensha-cybershake-fork/src/test/resources/org/opensha/sha/cybershake/plot/HazardCurvePlotter/USC_mod_probs_Mojave_Coachella.csv";
+	final String REF_CSV = "/Users/bhatthal/git/opensha-cybershake-fork/src/test/resources/org/opensha/sha/cybershake/plot/HazardCurvePlotter/USC_reference_probs.csv";
+	final String OUTPUT_DIR = "/Users/bhatthal/git/opensha-cybershake-fork/src/test/resources/org/opensha/sha/cybershake/plot/HazardCurvePlotter";
+	final String PLOT_CHARS_FILE = "/Users/bhatthal/git/opensha-cybershake-fork/src/main/resources/org/opensha/sha/cybershake/conf/robPlot.xml";
+	final String COMPONENT = "RotD50";
 
-	// We can compare using the Point2DComparator and set tolerance for how "approximate".
+	HazardCurvePlotter plotter;
+	
+//	final String[] FETCH_ARGS = {
+//			"-o", OUTPUT_DIR + "/fetchdb/1",
+//			"-R", RUN_ID,
+//			"-cmp", COMPONENT,
+//			"--plot-chars-file", PLOT_CHARS_FILE,
+//			"-t", "TXT,PDF"
+//	};
+	
+	@Before
+	public void setup() {
+		// Create a HazardCurvePlotter
+		this.plotter = new HazardCurvePlotter(Cybershake_OpenSHA_DBApplication.getDB());
+		// Ensure directories {fetchdb,modprob,refprob}/{1,2} exist and no cached results
+	}
+	
+
+	// TODO: Read curve data from TXT type output
+
+	// TODO: Create CommandLine option,
+	//       mock user input by manually passing String[] args.
+	// plotCurvesFromOptions
+	
+	// TODO: We can compare using the Point2DComparator and set tolerance for how "approximate".
 
 	// Hazard Curve x,y points for the plots are consistent when fetching from DB
 	@Test
